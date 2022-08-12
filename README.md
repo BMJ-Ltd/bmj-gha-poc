dd554a6a4d08e5da80f5dbd31e140929eb3fe7c6
mmyrtek <mmyrtek@bmj.com>
2022-08-12 09:25:36 +0100

    erro handling for empty pointer added

--- README.md
+++ README.md
@@ -1,20 +1,57 @@
-# bmj-gha-poc
-```
-name: Tester-Run
-on: [push, pull_request]
-jobs:
-  build:
-    name: Get Last Version Number
-    runs-on: <your runner>
-    steps:
-      - uses: actions/checkout@v1
 
-      - name: BMJ Custom Github Action
-        uses: BMJ-Ltd/bmj-gha-poc@1.0.13
+# BMJ
+
+This GHA will check a given repository and increment a version number
+
+The version number must be in the format of major.minot.patch like 1.2.3
+
+If no such versionis found on any of the images that are alredy in the ECR a new version number 0.0.1 
+will be created by default.
+
+
+
+
+## Environment Variables
+
+To run this project, you will need to have all the correct GitHub secrets set to access the ECR in AWS
+
+
+## Usage/Examples
+
+The two required parameters are the ***ecr_name*** and the ***patch***
+
+Valid parameters for patch are 
+* major
+* minor
+* patch
+
+Where ***major*** will increase the ***major*** version and set the ***minor*** and ***patch*** to zero
+
+Where ***minor*** will increase the ***minor*** version and set the ***patch*** to zero
+
+Where ***patch*** will increase the ***patch*** version
+
+### Add this to your gitHubWorkflow
+
+
+```
+
+       ## Version Number increase
+      - name: BMJ Version number increment
+        uses: BMJ-Ltd/xxxxx@1.0.40
         id: version
         with:
-          ecr_name: <ecr_name>
+          ecr_name: ${{ github.event.repository.name }}
+          version_type: "patch"
       - name: Get the version
         run: |
-          echo "${{ steps.version.outputs.myOutput }}"
-```          
+          echo '${{ steps.version.outputs.newVersion }}'
+
+```
+
+
+# Hi, I'm Mike! 👋
+
+
+## 🚀 About Me
+I'm a DevOps Engineer in BMJ
--- main.go
+++ main.go
@@ -13,9 +13,16 @@ import (
 	"github.com/aws/aws-sdk-go/service/ecr"
 )
 
+//------------------------------------------------------------------------------------
+// VARIABLES
+//------------------------------------------------------------------------------------
 var major, minor, patch int
 
-// function increment patch number
+//------------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------------
+// FUNCTION INCREMENT PATCH NUMBER
+//------------------------------------------------------------------------------------
 func incrementPatch() {
 	patch++
 }
@@ -29,12 +36,20 @@ func incrementMajor() {
 	patch = 0
 }
 
-// function to append a string to a slice of strings
+//------------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------------
+// FUNCTION TO APPEND A STRING TO A SLICE OF STRINGS
+//------------------------------------------------------------------------------------
 func appendString(slice []string, data string) []string {
 	return append(slice, data)
 }
 
-// function to parse version number
+//------------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------------
+// FUNCTION TO PARSE VERSION NUMBER
+//------------------------------------------------------------------------------------
 func parseVn(n string) (string, error) {
 	re := regexp.MustCompile(`^(\d+)\.(\d+)\.(\d+)$`)
 	match := re.FindStringSubmatch(n)
@@ -45,14 +60,23 @@ func parseVn(n string) (string, error) {
 	major, _ = strconv.Atoi(regexp.MustCompile(`\D`).ReplaceAllString(match[1], ""))
 	minor, _ = strconv.Atoi(regexp.MustCompile(`\D`).ReplaceAllString(match[2], ""))
 	patch, _ = strconv.Atoi(regexp.MustCompile(`\D`).ReplaceAllString(match[3], ""))
-	return fmt.Sprintf("Incrementing: %v.%v.%v", major, minor, patch), nil
+	return fmt.Sprintf("Incrementing this version: %v.%v.%v\n", major, minor, patch), nil
 
 }
 
+//------------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------------
+// MAIN
+//------------------------------------------------------------------------------------
 func main() {
-	//variables
+
+	// Get the input Variables
 	repositoryName := os.Getenv("INPUT_ECR_NAME")
 	versionType := os.Getenv("INPUT_VERSION_TYPE")
+
+	// for debugging purposes this can be run from the commandline if the values below are used
+	// and the input variables are commented out
 
 	//repositoryName := "activity-api"
 	//versionType := "patch"
@@ -62,6 +86,10 @@ func main() {
 	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String("eu-west-1")},
 	)
+	if err != nil {
+		fmt.Println("Error creating session:", err)
+		return
+	}
 
 	// Create ECR Service Client
 	svc := ecr.New(sess)
@@ -86,7 +114,7 @@ func main() {
 			}
 		} else {
 			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
+
 			fmt.Println(err.Error())
 		}
 		return
@@ -94,14 +122,19 @@ func main() {
 	//loop through the images and append the version number to a slice
 	for _, image := range result.ImageIds {
 
-		// only add the version number to the slice if it is a valid version number
+		//fmt.Printf("DEBUG ====> I got to here %s\n", image)
 
-		re := regexp.MustCompile(`^(\d+)\.(\d+)\.(\d+)$`)
-		match := re.FindStringSubmatch(*image.ImageTag)
-		if match != nil {
-			slice = appendString(slice, *image.ImageTag)
+		// must ignore ImageTag if it does not exist
+		if image.ImageTag != nil {
+			//fmt.Printf("DEBUG ====> I got to here too %s\n", *image.ImageTag)
+			// only add the version number to the slice if it is a valid version number
+			re := regexp.MustCompile(`^(\d+)\.(\d+)\.(\d+)$`)
+			match := re.FindStringSubmatch(*image.ImageTag)
+			if match != nil {
+
+				slice = appendString(slice, *image.ImageTag)
+			}
 		}
-
 	}
 	// if lenght slice == 0 then exit the program
 	if len(slice) == 0 {
@@ -126,15 +159,8 @@ func main() {
 
 	}
 
-	// create the new version number
-	//newVersion := fmt.Sprintf("%v.%v.%v", major, minor, patch)
-
-	// if lenght imageIds is 0 then exit
-	// if len(result.ImageIds) == 0 {
-	// fmt.Println("No images found")
-	// return
-	// }
-
 	fmt.Println(fmt.Sprintf(`::set-output name=newVersion::%s`, fmt.Sprintf("%v.%v.%v", major, minor, patch)))
 
 }
+
+//------------------------------------------------------------------------------------
